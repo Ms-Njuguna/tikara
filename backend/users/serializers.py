7999e6fd1c254b7a75a481f600dbc0ab6e62f083
Patricia <patricianjuguna084@gmail.com>
2026-03-25 21:18:08 +0300

    feat: allow users to register as organizers via serializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,15 +5,17 @@ from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    is_organizer = serializers.BooleanField(default=False)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'is_organizer')
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            is_organizer=validated_data.get('is_organizer', False)
         )
         return user
