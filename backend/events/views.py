a14cd866ba00058d6226b32628c204ccb91f992e
Patricia <patricianjuguna084@gmail.com>
2026-03-25 11:41:58 +0300

    feat: implement get and create event API endpoints

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,6 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Event
+from .serializers import EventSerializer
 
+
+# GET all events
 @api_view(['GET'])
-def test_api(request):
-    return Response({"message": "TIKARA backend is working 🚀"})
+def get_events(request):
+    events = Event.objects.all()
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data)
+
+
+# CREATE event
+@api_view(['POST'])
+def create_event(request):
+    serializer = EventSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
