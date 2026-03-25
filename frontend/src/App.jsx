9a2ec1ef0b686661f7b4aec0e1ae4599a6e25c29
Patricia <patricianjuguna084@gmail.com>
2026-03-25 21:10:05 +0300

    refactor(routing): remove routing from App.jsx and into AppRoutes.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { useRoutes } from "react-router-dom";
+import { AppRoutes } from "./routes/AppRoutes"
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/events/test/")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
-  return (
-    <div>
-      <h1 class="text-3xl font-bold underline">TIKARA 🎟️</h1>
-      <p>{message}</p>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
-  );
+  return useRoutes(AppRoutes)
 }
 
-export default App;
+export default App
