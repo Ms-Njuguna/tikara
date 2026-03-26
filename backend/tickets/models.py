d7a5bf1e141e91f29a57c27fc583fc8b5e53a09c
Patricia <patricianjuguna084@gmail.com>
2026-03-26 10:49:53 +0300

    refactor: separate TicketType and Ticket models for scalable ticket system

--- backend/tickets/models.py
+++ backend/tickets/models.py
@@ -4,7 +4,7 @@ from events.models import Event
 
 User = settings.AUTH_USER_MODEL
 
-class Ticket(models.Model):
+class TicketType(models.Model):
     TICKET_TYPE_CHOICES = (
         ('earlybird', 'Earlybird'),
         ('regular', 'Regular'),
@@ -13,11 +13,24 @@ class Ticket(models.Model):
         ('group', 'Group'),
     )
 
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='ticket_types')
+
+    name = models.CharField(max_length=20, choices=TICKET_TYPE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    quantity = models.IntegerField()  # total available
+
+    # FOR GROUP TICKETS
+    group_size = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.event} - {self.name}"
+
+class Ticket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='tickets')
 
-    ticket_type = models.CharField(max_length=10, choices=TICKET_TYPE_CHOICES)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    ticket_type = models.ForeignKey(TicketType, on_delete=models.CASCADE)
 
     is_paid = models.BooleanField(default=False)
 
